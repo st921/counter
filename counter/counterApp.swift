c6a41fd20e69221b40b9d8435429c37ed6b33cbe
st921 <syou.tana0921@icloud.com>
2026-02-08 19:01:53 +0900

    Initial Commit

---
+++ counter/counterApp.swift
@@ -0,0 +1,17 @@
+//
+//  counterApp.swift
+//  counter
+//
+//  Created by しょう on 2026/02/08.
+//
+
+import SwiftUI
+
+@main
+struct counterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
